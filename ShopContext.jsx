cf72088e601e719cd02b5a85159560af1220f626
shubhangiherode <154736505+shubhangiherode@users.noreply.github.com>
2025-02-17 20:36:27 +0530

    Add files via upload

---
+++ ShopContext.jsx
@@ -0,0 +1,88 @@
+ import { createContext, useEffect, useState } from "react";
+import { products } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+  /* Currency symbol */
+  const currency = "$";
+  const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
+
+
+
+  // Add to cart function
+  const addToCart = async (itemId, size) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  // Update cart quantity
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
+
+  // Get total cart amount
+  const getCartAmount = () => {
+    let totalAmount = 0;
+
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+
+      if (itemInfo) {
+        for (const item in cartItems[items]) {
+          try {
+            if (cartItems[items][item] > 0) {
+              totalAmount += itemInfo.price * cartItems[items][item];
+            }
+          } catch (error) {
+            console.error("Error calculating total amount:", error);
+          }
+        }
+      }
+    }
+    
+    return totalAmount;
+  };
+
+  // Context value
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItems,
+    addToCart,
+    updateQuantity,
+    getCartAmount,navigate
+  };
+
+  return <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>;
+};
+
+export default ShopContextProvider;
